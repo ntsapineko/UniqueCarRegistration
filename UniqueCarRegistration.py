f4cb5ab06bfff9b9c6d57ee1d53772740f67e447
datazarpy <antonvichon@gmail.com>
2025-11-04 13:16:27 +0200

    Update UniqueCarRegistration.py

--- UniqueCarRegistration.py
+++ UniqueCarRegistration.py
@@ -5,26 +5,36 @@ import string
 class UniqueCarRegistration:
     def __init__(self):
         self.used = list()
-        self.possible_count = (
-            (26**2) * (10**2) * (26**2)
-        )  # Total possible combinations (26 alphabets and 10 digits)
+
+        twobytwo_combo = (26**2) * (10**2) * (26**2) * 9
+        threebythree_combo = (26**3) * (10**3) * 9
+        # Total possible combinations (26 alphabets and 10 digits and 9 provinces)
+        self.possible_count = twobytwo_combo + threebythree_combo
 
     def generate(self):
         prov_list = ["GP", "NW", "FS", "MP", "NC", "EC", "ZN", "WC", "L"]
-
-        prov = "".join(random.choices(prov_list))
-        part1 = "".join(random.choices(string.ascii_uppercase, k=2))
-        part2 = "".join(random.choices(string.digits, k=2))
-        part3 = "".join(random.choices(string.ascii_uppercase, k=2))
-        sep = random.choice([" ", "-"])
-
-        candidate_reg = f"{part1}{sep}{part2}{sep}{part3}{sep}{prov}"
 
         # To avoid repeated combinations
         if len(self.used) >= self.possible_count:
             raise RuntimeError("All possible combinations have been exhausted.")
 
         while True:
+            prov = "".join(random.choice(prov_list))
+            sep = random.choice([" ", "-"])
+
+            if random.choice([True, False]):
+                two_part1 = "".join(random.choices(string.ascii_uppercase, k=2))
+                two_part2 = "".join(random.choices(string.digits, k=2))
+                two_part3 = "".join(random.choices(string.ascii_uppercase, k=2))
+                candidate_reg = (
+                    f"{two_part1}{sep}{two_part2}{sep}{two_part3}{sep}{prov}"
+                )
+
+            else:
+                three_part1 = "".join(random.choices(string.ascii_uppercase, k=3))
+                three_part2 = "".join(random.choices(string.digits, k=3))
+                candidate_reg = f"{three_part1}{sep}{three_part2}{sep}{prov}"
+
             if candidate_reg not in self.used:
                 self.used.append(candidate_reg)
                 break
