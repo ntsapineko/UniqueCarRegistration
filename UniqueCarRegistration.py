21d31424a3c9b0efdce89550abb7b1aa8c5612e1
datazarpy <antonvichon@gmail.com>
2025-11-04 12:11:53 +0200

    Update UniqueCarRegistration.py

--- UniqueCarRegistration.py
+++ UniqueCarRegistration.py
@@ -4,55 +4,35 @@ import string
 
 class UniqueCarRegistration:
     def __init__(self):
-        self.used = set()
+        self.used = list()
         self.possible_count = (
             (26**2) * (10**2) * (26**2)
         )  # Total possible combinations (26 alphabets and 10 digits)
 
     def generate(self):
-        print(
-            "Choose your province by number: \n 1 for Gauteng\n 2 for North West\n3 for Free State\n 4 for Mpumalanga\n5 for Norther Cape\n6 for Eastern Cape\n7 for KwaZulu Natal\n8 for Western Cape\n9 for Limpopo"
-        )
-        choice_prov = input()
-        prov = ""
+        prov_list = ["GP", "NW", "FS", "MP", "NC", "EC", "ZN", "WC", "L"]
 
-        if choice_prov == "1":
-            prov = "GP"
-        elif choice_prov == "2":
-            prov = "NW"
-        elif choice_prov == "3":
-            prov = "FS"
-        elif choice_prov == "4":
-            prov = "MP"
-        elif choice_prov == "5":
-            prov = "NC"
-        elif choice_prov == "6":
-            prov = "EC"
-        elif choice_prov == "7":
-            prov = "ZN"
-        elif choice_prov == "8":
-            prov = "WC"
-        elif choice_prov == "9":
-            prov = "L"
-        else:
-            print("Invalid choice")
+        prov = "".join(random.choices(prov_list))
+        part1 = "".join(random.choices(string.ascii_uppercase, k=2))
+        part2 = "".join(random.choices(string.digits, k=2))
+        part3 = "".join(random.choices(string.ascii_uppercase, k=2))
+        sep = random.choice([" ", "-"])
+
+        candidate_reg = f"{part1}{sep}{part2}{sep}{part3}{sep}{prov}"
 
         # To avoid repeated combinations
         if len(self.used) >= self.possible_count:
             raise RuntimeError("All possible combinations have been exhausted.")
 
         while True:
-            part1 = "".join(random.choices(string.ascii_uppercase, k=2))
-            part2 = "".join(random.choices(string.digits, k=2))
-            part3 = "".join(random.choices(string.ascii_uppercase, k=2))
-            sep = random.choice([" ", "-"])
-            candidate_reg = f"{part1}{sep}{part2}{sep}{part3}+ ' '+{prov}"
-
             if candidate_reg not in self.used:
-                self.used.add(candidate_reg)
-                return candidate_reg
+                self.used.append(candidate_reg)
+                break
+
+        print(candidate_reg)
 
 
-gen = UniqueCarRegistration()
-for _ in range(5):
-    print(gen.generate())
+if __name__ == "__main__":
+    car_reg = UniqueCarRegistration()
+    car_reg.generate()
+    print()
